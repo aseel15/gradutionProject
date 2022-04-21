4d83bed313a2d86f84d38623f145bf7a5e7239ce
aseel15 <aseel.zbai@gmail.com>
2022-04-21 22:17:05 +0300

    edit commit

---
+++ app/src/main/java/com/example/graduationproject/TripMapActivity.java
@@ -0,0 +1,144 @@
+package com.example.graduationproject;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class TripMapActivity extends AppCompatActivity {
+    TextView jericoCircle, jewsCircle, MHCircle, EArrivingTimeTxt, ArrivingTimeTxt, jericoClockTxt, jewsClockTxt, MHClockTxt;
+
+    //This is Google Location Service API, this is the heart of our application.
+    FusedLocationProviderClient fusedLocationProviderClient;
+    // this is configuration class that tells FusedLocationProviderClient how to work
+    LocationRequest locationRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_map);
+
+        jericoCircle = findViewById(R.id.jericoCircle);
+        jewsCircle = findViewById(R.id.jewsCircle);
+        MHCircle = findViewById(R.id.MHCircle);
+
+        EArrivingTimeTxt = findViewById(R.id.EArrivingTimeTxt);
+        ArrivingTimeTxt = findViewById(R.id.ArrivingTimeTxt);
+
+        jericoClockTxt = findViewById(R.id.jericoClockTex);
+        jewsClockTxt = findViewById(R.id.JewsClockTxt);
+        MHClockTxt = findViewById(R.id.MHClockTxt);
+
+
+        locationRequest =  LocationRequest.create();
+
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        updateGPS();
+
+    }
+   /* public void gps_onClick(View view) {
+        if(sw_gps.isChecked()){
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            tv_sensor.setText("Using GPS");
+        }
+        else{
+            locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+            tv_sensor.setText("Using WiFI and Towers");
+        }
+    }*/
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 99){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                updateGPS();
+            }
+            else{
+                Toast.makeText(this, "this app requires permissions to work properly", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private void updateGPS(){
+        //get permissions from the user to track gps
+        //get current location from fused client
+        //update UI
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(TripMapActivity.this);
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this,
+                    new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            updateUI(location);
+                        }
+                    });
+        }
+        else{
+            //permission not granted, we will ask for it
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 99);
+            }
+        }
+    }
+
+    private void updateUI(Location location) {
+        //it will determine the current address of the location & the current time
+        try {
+            Geocoder geocoder = new Geocoder(TripMapActivity.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            String address = addresses.get(0).getAddressLine(0);
+            String currentTime = new SimpleDateFormat("hh:mm a", Locale.getDefault()).format(new Date());
+
+            if(address.equalsIgnoreCase(/*"General Administration of crossings and borders"*/"obwain")){
+                jericoCircle.setBackgroundResource(R.drawable.visited);
+                jericoClockTxt.setText(String.valueOf(currentTime));
+            }
+            if(address.equalsIgnoreCase("King Hussein Bridge Border Crossing")){//allenby bridge
+                jewsCircle.setBackgroundResource(R.drawable.visited);
+                jewsClockTxt.setText(String.valueOf(currentTime));
+            }
+            if(address.equalsIgnoreCase("King Hussein Border Crossing")){
+                MHCircle.setBackgroundResource(R.drawable.visited);
+                MHClockTxt.setText(String.valueOf(currentTime));
+                ArrivingTimeTxt.setText(currentTime);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
